01ed48d197a47e8ded5bb98c6c2bf47aa8970bae
Akhil27P <akhil.edu27@gmail.com>
2025-06-04 00:49:32 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Portfolio from "./Portfolio";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <Portfolio />
