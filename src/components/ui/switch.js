004617a108355bf46ad7045209f30d29af661856
Akhil27P <akhil.edu27@gmail.com>
2025-06-13 06:36:36 -0400

    switch.js

---
+++ src/components/ui/switch.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export function Switch({ checked, onCheckedChange }) {
+  return (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onCheckedChange(e.target.checked)}
+        className="sr-only peer"
+      />
+      <div className="w-11 h-6 bg-gray-400 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+    </label>
+  );
+}
