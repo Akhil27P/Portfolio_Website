ec062d402061c47e4236427ab3b37b14b9eba5ff
Akhil27P <akhil.edu27@gmail.com>
2025-06-13 07:01:41 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Portfolio from "./Portfolio";
+import RedirectPage from "./RedirectPage";  // new component to handle slug redirects
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Portfolio />
+    <BrowserRouter basename={process.env.PUBLIC_URL || "/"}>
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path="/:slug" element={<RedirectPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
